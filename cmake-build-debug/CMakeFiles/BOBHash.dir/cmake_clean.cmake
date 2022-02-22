f4233e05619bcc3985efb882261ec878a5f3bdfd
fsrmmomo <1906994189@qq.com>
2022-02-22 16:44:52 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/BOBHash.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BOBHash.exe"
+  "BOBHash.exe.manifest"
+  "BOBHash.pdb"
+  "CMakeFiles/BOBHash.dir/src/CPU/common/BOBHash32.cpp.obj"
+  "libBOBHash.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BOBHash.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
